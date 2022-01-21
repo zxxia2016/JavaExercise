1029361374dadfc63462c0fc39c16ac363a0b811
zxxia <zxxia2016@gmail.com>
2022-01-21 23:59:50 +0800

    demo

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+package com.zxxia.hello;
+
+import java.util.Arrays;
+
+/**
+ * zxxia
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] array = {5, 4 , 3, 0, 6};
+        int length = array.length;
+        for (int i = 0; i < length-1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int a = array[i];
+                int b = array[j];
+                if (a > b) {
+                    int c = array[j];
+                    array[j] = a;
+                    array[i] = c;
+                }
+            }
+        }
+        //输出数组
+        System.out.println(Arrays.toString(array));
+    }
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// 编译命令 javac HelloWorld.java，生成.class
+// 运行Java程序 java HelloWorld
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}

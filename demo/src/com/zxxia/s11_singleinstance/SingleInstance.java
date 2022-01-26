9b47c77ed1ab7e68563ca74e82485145ac546ab7
zxxia <zxxia2016@gmail.com>
2022-01-26 17:41:11 +0800

    修改目录名称

--- demo/src/com/zxxia/s11_singleinstance/SingleInstance.java
+++
@@ -1,5 +0,0 @@
-package com.zxxia.s11_singleinstance;
-
-public class SingleInstance {
-
-}
---
+++ demo/src/com/zxxia/s12_extents/ExtendsTest.java
@@ -0,0 +1,14 @@
+package com.zxxia.s12_extents;
+
+class people {
+    String name;
+    String age;
+}
+class Student extends people{
+
+}
+public class ExtendsTest {
+    public static void main(String[] args) {
+        new Student();
+    }
+}

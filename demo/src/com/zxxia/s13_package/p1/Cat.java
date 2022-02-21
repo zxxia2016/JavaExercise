7de9edff1683db95f4c836a9566d54c55a2524a8
zxxia <zxxia2016@gmail.com>
2022-02-21 11:46:50 +0800

    包、枚举、final关键字

---
+++ demo/src/com/zxxia/s13_package/p1/Cat.java
@@ -0,0 +1,10 @@
+package com.zxxia.s13_package.p1;
+
+public class Cat {
+    public void log() {
+        System.out.println("Cat 1");
+    }
+    void run() {
+
+    }
+}
---
+++ demo/src/com/zxxia/s13_package/p2/Cat.java
@@ -0,0 +1,7 @@
+package com.zxxia.s13_package.p2;
+
+public class Cat {
+    public void log() {
+        System.out.println("Cat 2");
+    }
+}

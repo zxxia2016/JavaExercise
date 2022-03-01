b67f77a515346189a0a7453fdbaa78b5a02180af
zxxia2016 <zxxia2016@gmail.com>
2022-03-01 10:10:45 +0800

    内部类和Object对象对象学习

--- demo/src/com/zxxia/s19_insideclass/Test.java
+++ demo/src/com/zxxia/s19_insideclass/Test.java
@@ -3,12 +3,13 @@ package com.zxxia.s19_insideclass;
 /**
  * 内部类：理解成（寄生），外部类理解成（宿主）
  * 1. 分类：
- *  --静态内部类（了解）:跟普通类没差别,只能访问外部类的静态成员变量
- *  --成员内部类（了解）
- *  --局部内部类（了解）
- *  --匿名内部类（重点）
+ *  --静态内部类（了解）:跟普通类没差别,只能访问外部类的静态成员变量；用法：外部类名.内部类名 对象名=new外部类名.内部类构造器
+ *  --成员内部类（了解）:要先new外部类；才能再new内部类；更符合现实场景；用法：外部类名.内部类名 对象名=new外部类构造器.new 内部构造器
+ *  --局部内部类（了解）：鸡肋，不了解
+ *  --匿名内部类（重点）：作用：直接创建子类对象，简化代码
  */
 
+// 静态内部类
 class OuterClass {
     private static String name;
 
@@ -20,9 +21,57 @@ class OuterClass {
         }
     }
 }
-public class Test {
-    public static void main(String[] args) {
-        OuterClass.InnerClass innerClass = new OuterClass.InnerClass();
-        innerClass.show();
+
+// 成员内部类：人和心脏的关系
+class OuterClass1 {
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+    private static String sName;
+
+    public class InnnerClass {
+        private String name;
+        //不允许使用
+//        private static int age;
+        void showName(){
+            System.out.println(this.name);
+            //内部类调用外部类变量
+            System.out.println(OuterClass1.this.name);
+            System.out.println(getName());
+            System.out.println(sName);
+        }
+        //不允许使用
+//        static void showAge(){
+//
+//        }
     }
 }
+
+abstract class Animal{
+    public abstract void run();
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+        //静态内部类
+        OuterClass.InnerClass innerClass = new OuterClass.InnerClass();
+        innerClass.show();
+
+        // 成员内部类
+        OuterClass1.InnnerClass i = new OuterClass1().new InnnerClass();
+        i.showName();
+//        OuterClass1.InnnerClass.showAge();
+
+        //匿名内部类
+        Animal a = new Animal() {
+            public void run(){
+                System.out.println("run");
+            }
+        };
+        a.run();
+    }
+}
+
---
+++ demo/src/com/zxxia/s20_object/Test.java
@@ -0,0 +1,36 @@
+package com.zxxia.s20_object;
+
+import java.util.Objects;
+
+/**
+ * Object祖宗类，JDK1.7才有
+ * 1. toString方法，返回类名权限名内存地址,这返回值毫无意义；但重写之后就有意义，可以看到想的信息，例如Student:toString
+ * 2. equals方法，比较地址是否相同；也是重写后，内容更丰富
+ */
+class Student //extends Object //等同于继承Object
+{
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Student{ name:" +this.name +"}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.toString());// com.zxxia.s20_object.Student@1b6d3586
+        System.out.println(s);// com.zxxia.s20_object.Student@1b6d3586
+        System.out.println(s.getClass());
+    }
+}

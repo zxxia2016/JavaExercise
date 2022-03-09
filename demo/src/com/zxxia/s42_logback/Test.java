3e0a8cfaf8158913be983cbd52345aa56eb0ac5a
zxxia2016 <zxxia2016@gmail.com>
2022-03-09 10:16:10 +0800

    logback

---
+++ demo/src/com/zxxia/s42_logback/Test.java
@@ -0,0 +1,45 @@
+package com.zxxia.s42_logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 日志技术：Logback:https://logback.qos.ch/
+ * 1. 优点
+ * ---写入文件或数据库
+ * ---取消日志，不需要修改代码，灵活性高
+ * ---多线程，性能好
+ * 2. 体系结构
+ * ---日志规范
+ * ------Commons Logging，简称：JCL，sun公司
+ * ------Simple Logging Facae for Java，简称：slf4j
+ * ---日志实现框架
+ * ------Log4j
+ * ------JUL:java.util.logging
+ * ------Logback（Log4j升级版）
+ * ------其他
+ * 3. Logback:
+ * ---logback-core:核心模块，是另2个模块的基础
+ * ---logback-classic：log4的改良版，同时实现了slf4j api
+ * ---logback-access：与Tomcat和Jetty等Servlet容器集成，以提供HTTP访问日志功能
+ * ---logback.xml配置文件
+ * ------日志文件位置：控制台、文件
+ * ------日志文件日期
+ */
+public class Test {
+    public static final Logger logger = LoggerFactory.getLogger("Test");
+    public static void main(String[] args) {
+        test();
+    }
+    public static void test() {
+        try {
+            logger.debug("111");
+            logger.debug("222");
+            logger.info("333");
+            logger.trace("444");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("error:" + e);
+        }
+    }
+}

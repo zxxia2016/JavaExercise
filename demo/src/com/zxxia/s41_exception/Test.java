f7123e1d09fc0bd5ce4cf87aae3db2b99b56f4c0
zxxia2016 <zxxia2016@gmail.com>
2022-03-09 09:43:46 +0800

    Exception

--- demo/src/com/zxxia/s41_exception/Test.java
+++ demo/src/com/zxxia/s41_exception/Test.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * 异常：数组越界，空指针，数学操作、类型转换、数字转换等
@@ -17,10 +18,13 @@ import java.util.Date;
  * ---Exception
  * ------RuntimeException，例子：RumtimeExceptionTest
  * ------RuntimeException之外的异常: 编译时异常：日期格式化异常,例子：CompileExceptionTest
- * ------------出现异常抛给调用者 throws Exception
- * ------------出现异常自己捕获处理 try... catch...
- * ------------两者相结合
- *
+ * ------------出现异常抛给调用者 throws Exception。例子：CompileExceptionTest.run
+ * ------------出现异常自己捕获处理 try... catch..。例子：CompileExceptionTest.run1
+ * ------------两者相结合:内部抛出异常，调用层统一处理；例子：ExceptionTest.run
+ * 2. 案例：死循环输入测试，例子：InputTest
+ * 3. 自定义异常：
+ * ---Exception,编译时异常
+ * ---RuntimeException,运行时异常
  */
 
 class RumtimeExceptionTest implements iTest {
@@ -48,6 +52,7 @@ class RumtimeExceptionTest implements iTest {
         // Integer.valueOf(number);
     }
 }
+
 class CompileExceptionTest {
     void run() throws ParseException, FileNotFoundException {
         String date = "2015-12-12 10:00:00";
@@ -56,16 +61,102 @@ class CompileExceptionTest {
         Date d = simpleDateFormat.parse(date);
         System.out.println(d);
 
-         // InputStream inputStream = new FileInputStream("E:/美女.png");
+        // InputStream inputStream = new FileInputStream("E:/美女.png");
     }
+
     void run1() {
         try {
             InputStream inputStream = new FileInputStream("E:/美女.png");
-        } catch ( FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 }
+
+class ExceptionTest {
+    private void dateFormat() throws Exception {
+        String date = "2015-12-12 10:00:00";
+        // 自动添加 ParseException
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM-dd HH:mm:ss");
+        Date d = simpleDateFormat.parse(date);
+        System.out.println(d);
+    }
+
+    void run() {
+        try {
+            this.dateFormat();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class InputTest implements iTest {
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入价格");
+            try {
+                String str = scanner.next();
+                Double v = Double.valueOf(str);
+                System.out.println("输入价格正确");
+                break;
+
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.out.println("输入价格错误");
+
+            }
+
+        }
+    }
+}
+
+class CustomExceptionTest implements iTest {
+
+    //
+    class MyException extends Exception {
+        public MyException() {
+        }
+
+        public MyException(String message) {
+            super(message);
+        }
+    }
+
+    // 运行时异常
+    class MyRumtimeException extends RuntimeException {
+        public MyRumtimeException() {
+        }
+
+        public MyRumtimeException(String message) {
+            super(message);
+        }
+    }
+    public void checkAge(int age) throws MyException {
+        if (age < 0) {
+            throw new MyException();
+        }
+    }
+    public void checkAge2(int age){
+        if (age < 0) {
+            throw new MyRumtimeException();
+        }
+    }
+    @Override
+    public void run() {
+        try{
+            this.checkAge(-1);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.checkAge2(-1);
+    }
+}
+
 public class Test {
     public static void main(String[] args) throws Exception {
         RumtimeExceptionTest rumtimeExceptionTest = new RumtimeExceptionTest();
@@ -74,6 +165,15 @@ public class Test {
         CompileExceptionTest compileExceptionTest = new CompileExceptionTest();
         compileExceptionTest.run();
         compileExceptionTest.run1();
+
+        ExceptionTest exceptionTest = new ExceptionTest();
+        exceptionTest.run();
+
+        InputTest inputTest = new InputTest();
+        // inputTest.run();
+
+        CustomExceptionTest customExceptionTest = new CustomExceptionTest();
+        customExceptionTest.run();
         System.out.println("--------------------main end----------------------");
     }
 }

6b6483dbed8c1337704aa5d692e5d6e369329bfd
zxxia2016 <zxxia2016@gmail.com>
2022-03-16 09:27:20 +0800

    commons-io、nio

--- demo/src/com/zxxia/s47_commonsio/Test.java
+++ demo/src/com/zxxia/s47_commonsio/Test.java
@@ -1,9 +1,12 @@
 package com.zxxia.s47_commonsio;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * commons-io:Apache公司开发的
@@ -12,11 +15,21 @@ import java.io.FileOutputStream;
  * 1. 主要类
  * ---1. FileUtils
  * ---2. IOUtils
+ *
+ * nio：JAVA公司
+ * 
  */
 public class Test {
     public static void main(String[] args) throws Exception {
         //复制文件
         IOUtils.copy(new FileInputStream("file1.txt"), new FileOutputStream("file11.txt"));
+        // FileUtils.copyDirectory(new File("D:\\srcDir"), new File("d:\\DestDir"));
+        // 删除所有文件夹
+        // FileUtils.deleteDirectory("D:\\test");
 
+        // JDK1.7 NIO2.0
+        Files.copy(Path.of("D:\\srcDir"), Path.of("d:\\DestDir"));
+        //删除空文件夹
+        Files.deleteIfExists(Path.of("D:\\srcDir"));
     }
 }

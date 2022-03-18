f7d4c95c2a3d5f2d2dcc8bd530014e2c3d025ce1
zxxia <zxxia2016@gmail.com>
2022-03-19 06:47:18 +0800

    多线程并发、并行；线程声明周期；网络编程三要素、UDP

---
+++ demo/src/com/zxxia/s49_net/UDPClient.java
@@ -0,0 +1,9 @@
+package com.zxxia.s49_net;
+
+import java.net.DatagramSocket;
+
+public class UDPClient {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(8888);
+    }
+}

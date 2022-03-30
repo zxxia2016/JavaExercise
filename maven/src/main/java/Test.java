e80ba26e1e0cae0aa262f4dcd5c20f04ac1e3064
zxxia <zxxia2016@gmail.com>
2022-03-31 06:28:22 +0800

    maven

---
+++ maven/src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * maven环境配置
+ * 1. https://mvnrepository.com/
+ * 2. 目标：
+ * ------学会配置maven环境变量
+ * ------配置conf/setting文件：修改本地仓库位置：localRepository；新增镜像：mirrors
+ * ------创建maven项目；修改idea的maven的设置，仓库
+ * ------配置pom.xml：添加依赖：dependency；scope作用；
+ * ------安装maven help插件
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(" hello maven");
+    }
+}

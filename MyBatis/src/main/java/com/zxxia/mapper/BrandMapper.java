a838012256f7a0fd4b9885e499a6ab6d25f2bd5b
zxxia <zxxia2016@gmail.com>
2022-07-11 23:30:55 +0800

    Mybatis 多条件查询

--- MyBatis/src/main/java/com/zxxia/mapper/BrandMapper.java
+++ MyBatis/src/main/java/com/zxxia/mapper/BrandMapper.java
@@ -1,6 +1,7 @@
 package com.zxxia.mapper;
 
 import com.zxxia.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,4 +10,14 @@ public interface BrandMapper {
     List<Brand> selectAll();
 
     Brand selectById(int id);
+
+    /**
+     * 条件查询
+     * * 参数接收
+     *      1. 散装参数：@Param注解
+     *
+     */
+    List<Brand> selectByCondition(@Param("status") int status,
+                                  @Param("companyName") String companyName,
+                                  @Param("brandName") int brandName );
 }
--- MyBatis/src/main/test/java/com/zxxia/test/MyBatisTest.java
+++ MyBatis/src/main/test/java/com/zxxia/test/MyBatisTest.java
@@ -1,4 +1,4 @@
-package com.itheima.test;
+package com.zxxia.test;
 
 import com.itheima.mapper.BrandMapper;
 import com.itheima.pojo.Brand;

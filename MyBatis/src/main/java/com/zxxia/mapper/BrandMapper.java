345004832f1d0cfdbe011c72c17d0982b8983690
zxxia <zxxia2016@gmail.com>
2022-07-11 23:38:44 +0800

    Mybatis 多条件查询

--- MyBatis/src/main/java/com/zxxia/mapper/BrandMapper.java
+++ MyBatis/src/main/java/com/zxxia/mapper/BrandMapper.java
@@ -15,7 +15,8 @@ public interface BrandMapper {
      * 条件查询
      * * 参数接收
      *      1. 散装参数：@Param注解
-     *
+     *      2. 实体类封装参数：SQL语句中的参数和实体类属性名对应上
+     *      3. map集合：SQL语句中的参数和键名称对手
      */
     List<Brand> selectByCondition(@Param("status") int status,
                                   @Param("companyName") String companyName,
